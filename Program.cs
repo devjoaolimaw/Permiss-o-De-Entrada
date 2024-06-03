70b280395c66f70adc20870bc1dc98e2f1f8aaff
João Paulo <167019869+devjoaolimaw@users.noreply.github.com>
2024-06-02 21:52:55 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+Console.WriteLine("Qual sua idade? ");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Possui permissão caso menor de idade? ");
+string permissão = Console.ReadLine();
+bool ehMaiorDeIdade = idade >= 18;
+bool possuiAutorizacaoDoResponsavel = permissão == "sim";
+
+if (ehMaiorDeIdade)
+{
+    Console.WriteLine("Entrada liberada!");
+}
+else if (possuiAutorizacaoDoResponsavel)
+{
+    Console.WriteLine("Entrada liberada!");
+}
+else
+{
+    Console.WriteLine("Entrada não liberada");
+}
